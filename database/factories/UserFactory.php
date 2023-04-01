4eee8fe18a249f63849e486a88d44fdbeaa04c67
Lotrando <miroslav.klika@seznam.cz>
2023-04-01 10:13:02 +0200

    users factory and seeder code

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -14,12 +14,17 @@ class UserFactory extends Factory
      */
     public function definition()
     {
+        $last_name = $this->faker->lastName();
+        $first_name = $this->faker->firstName();
+
         return [
-            'name' => $this->faker->name(),
-            'email' => $this->faker->unique()->safeEmail(),
+            'login_name' => strtolower($last_name . '' . mb_substr($first_name, 0, 1)),
+            'last_name' => $last_name,
+            'first_name' => $first_name,
+            'email' => strtolower($last_name . '.' . $first_name) . '@' . $this->faker->unique()->domainName(),
             'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-            'remember_token' => Str::random(10),
+            'password' => bcrypt('password'),
+            'remember_token' => Str::random(15),
         ];
     }
 
